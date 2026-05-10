e96ea6d959940e813a803b617b340285759af414
shakina <shakinas129@gmail.com>
2026-05-10 22:45:05 +0530

    fixed mongodb atlas srv

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # MONGODB
 
-client = MongoClient("mongodb://admin:admin123@ac-rhm2tk6-shard-00-00.qmuuxio.mongodb.net:27017,ac-rhm2tk6-shard-00-01.qmuuxio.mongodb.net:27017,ac-rhm2tk6-shard-00-02.qmuuxio.mongodb.net:27017/?ssl=true&replicaSet=atlas-1ggbmg-shard-0&authSource=admin&appName=Cluster01")
+client = MongoClient("mongodb+srv://admin:admin123@cluster01.qmuuxio.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01")
 
 db = client["foodai_database"]
 
